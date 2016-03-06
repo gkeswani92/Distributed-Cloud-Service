c7c88639d3f0facef479699c2ef1f14d7ad3a93f
gkeswani92 <gauravkeswani92@gmail.com>
2016-03-06 16:44:04 -0500

    Modified Hello World flask file for AWS

--- Backend/run.py
+++ Backend/run.py
@@ -1,3 +1,11 @@
-#!flask/bin/python
-from services import app
-app.run(host='localhost',port=5000,debug=True)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+	app.secret_key = "super_secret_key"
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
