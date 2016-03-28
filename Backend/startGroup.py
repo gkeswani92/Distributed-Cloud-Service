d764b4dcc5de04c180205d7555055c57f5c4c50b
Natchaphon <natchaphon.r@gmail.com>
2016-03-28 14:49:55 -0400

    still need to debug Vsync.Action

--- Backend/startGroup.py
+++ Backend/startGroup.py
@@ -8,5 +8,27 @@ sys.path.append(os.getcwd()+'/../Resources')
 clr.AddReference('Vsync.dll')
 import Vsync
 
-#create a Vsync Group
-group = Vsync.Group("group1")
+
+Vsync.VsyncSystem.Start() # start Vsync system
+g = Vsync.Group("group1") # create a Vsync Group
+
+
+def myfunc(i):
+    print('Hello from myfunc with i=' + i.ToString())
+    return
+def myRfunc(r):
+    print('Hello from myRfunc with r=' + r.ToString())
+    group.Reply(-1)
+    return
+def myViewFunc(v):
+    print('New view: ' + v.ToString())
+    print ('My rank = ' + v.GetMyRank().ToString())
+    for a in v.joiners:
+        print('  Joining: ' + a.ToString() + ', isMyAddress='+a.isMyAddress().ToString())
+    for a in v.leavers:
+        print('  Leaving: ' + a.ToString() + ', isMyAddress='+a.isMyAddress().ToString())
+    return
+ 
+#g.RegisterHandler(0, Vsync.Action[int](myfunc))
+#g.RegisterHandler(1, Vsync.Action[float](myRfunc))
+g.RegisterViewHandler(Vsync.ViewHandler(myViewFunc))
