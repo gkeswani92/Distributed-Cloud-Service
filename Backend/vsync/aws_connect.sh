f52fc86791267b0bcc71579202bac079d55887c4
gkeswani92 <gauravkeswani92@gmail.com>
2016-04-12 16:26:24 -0400

    HAProxy works with flask. Now testing with VSync

---
+++ Backend/vsync/aws_connect.sh
@@ -0,0 +1,1 @@
+ssh -i "Handy.pem" ubuntu@ec2-54-208-106-213.compute-1.amazonaws.com
---
+++ Backend/vsync/createMasterServer.sh
@@ -0,0 +1,16 @@
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 1
+sleep 20
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 2
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 3
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 4
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 5
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 6
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 7
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 8
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 9
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 10
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 11
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 12
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 13
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 14
+mono ipy.exe /home/ubuntu/IronLanguages/Util/IronPython/ipy.exe masterServer.py 15
